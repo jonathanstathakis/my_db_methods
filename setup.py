ced8158ec3ec361cb583e583af3b476c0067193b
OctaneOolong <62565951+OctaneOolong@users.noreply.github.com>
2023-05-24 11:47:14 +1000

    updated gitignore, added requirements.txt, setup.py, my_db_methods.py files.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+from my_db_methods import __version__
+
+setup(
+    name="my_db_methods",
+    version=__version__,
+    url="https://github.com/jonathanstathakis/my_db_methods",
+    author="Jonathan Stathakis",
+    author_email="j.stathakis@live.com.au",
+    py_modules=["my_db_methods"],
+)
